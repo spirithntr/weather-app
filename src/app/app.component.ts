d729fa7ff0bb300d2d9b01e33e159110fc5e9f2b
iapast10 <iapast10@mts.ru>
2024-05-05 16:04:26 +0400

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wap-root',
+  standalone: true,
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'weather-app';
+}
